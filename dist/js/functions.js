63e16cc776de84d4b14ca07d8db5452945e00388
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2020-10-27 16:53:24 -0500

    now the navbar links close mini menu

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -7,3 +7,43 @@ const header = document.querySelector('.main-header');
             header.classList.remove('scrolled')
         }
     })
+
+    //Mini Menu close  
+    function handleNavClick() {
+        const navbar = document.querySelector('#menu-btn');
+        if (navbar.checked === true) {
+            this.onclick = closekNavbar;
+            
+        }else {
+            console.log('no esta activo el mini menu')
+            return
+        }
+    } 
+
+    function closekNavbar() {
+        const navbar = document.querySelector('#menu-btn')
+        if (navbar.checked !== null) {
+            navbar.checked = !navbar.checked
+            console.log(navbar.checked)
+        } 
+        
+    }
+
+
+    /* function check(checked = true) {
+        const cbs = document.querySelectorAll('input[name="color"]');
+        cbs.forEach((cb) => {
+            cb.checked = checked;
+        });
+    }
+    
+    function handleNavClick(e) {
+        const navLink = document.querySelectorAll('.nav-links')
+        
+    } 
+    
+    function closeNav() {
+        check(false);
+        // reassign click event handler
+        this.onclick = handleNavClick;
+    } */
